dc98edc4224b6f4c43ac44eb6d6017bc63a26abb
Renan Liberato <renan.libsantana@gmail.com>
2017-05-15 00:56:04 -0300

    Passed db initialization logic from app.php to controllers.php

--- src/app.php
+++ src/app.php
@@ -1,19 +1,10 @@
 <?php
 
 use Silex\Application;
-use Silex\Provider\DoctrineServiceProvider;
+
 use Silex\Provider\ServiceControllerServiceProvider;
 
 $app = new Application();
 $app->register(new ServiceControllerServiceProvider());
-
-$app->register(new DoctrineServiceProvider(), array(
-    'db.options' => array(
-        'driver'   => 'pdo_sqlite',
-        'path'     => __DIR__.'/../app.db',
-    ),
-));
-
-$app['db']->connect();
 
 return $app;
--- src/controllers.php
+++ src/controllers.php
@@ -5,6 +5,23 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Silex\Provider\DoctrineServiceProvider;
+
+$db = __DIR__.'/../db/';
+
+if ($app['session.test']) {
+    $db .= 'app_test.db';
+} else {
+    $db .= 'app.db';
+}
+
+$app->register(new DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver'   => 'pdo_sqlite',
+        'path'     => $db,
+    ),
+));
+$app['db']->connect();
 
 //Request::setTrustedProxies(array('127.0.0.1'));
 
