b8b473655d95bd5148da9e6ee042a2d1509fb481
Renan Liberato <renan.libsantana@gmail.com>
2017-05-15 00:14:31 -0300

    Removed twig settings and added doctrine configurations

--- src/app.php
+++ src/app.php
@@ -1,20 +1,19 @@
 <?php
 
 use Silex\Application;
-use Silex\Provider\AssetServiceProvider;
-use Silex\Provider\TwigServiceProvider;
+use Silex\Provider\DoctrineServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
-use Silex\Provider\HttpFragmentServiceProvider;
 
 $app = new Application();
 $app->register(new ServiceControllerServiceProvider());
-$app->register(new AssetServiceProvider());
-$app->register(new TwigServiceProvider());
-$app->register(new HttpFragmentServiceProvider());
-$app['twig'] = $app->extend('twig', function ($twig, $app) {
-    // add custom globals, filters, tags, ...
 
-    return $twig;
-});
+$app->register(new DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver'   => 'pdo_sqlite',
+        'path'     => __DIR__.'/../app.db',
+    ),
+));
+
+$app['db']->connect();
 
 return $app;
