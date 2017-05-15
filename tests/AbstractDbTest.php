54af466e43645fe147f7f4afd0c7061edd7c38ec
Renan Liberato <renan.libsantana@gmail.com>
2017-05-15 00:56:21 -0300

    Added db tests to ensure that a valid user is on test db on every test

---
+++ tests/AbstractDbTest.php
@@ -0,0 +1,62 @@
+<?php
+
+use Silex\WebTestCase;
+
+class AbstractTest extends WebTestCase
+{
+
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../src/app.php';
+
+        require __DIR__.'/../config/dev.php';
+
+        $app['session.test'] = true;
+
+        require __DIR__.'/../src/controllers.php';
+
+        $this->app = $app;
+
+        $this->populateUserTable();
+
+        return $this->app;
+    }
+
+    public function testApplicationLoadedTestDb()
+    {
+        $dbPath = $this->app['db']->getParams()['path'];
+
+        $this->assertEquals('app_test.db', basename($dbPath));
+    }
+
+    public function testDatabaseHasAnUser()
+    {
+        $conn = $this->app['db'];
+
+        $users = $conn->fetchAll('SELECT * FROM user');
+
+        $this->assertEquals(1, count($users));
+    }
+
+    public function tearDown()
+    {
+        $this->eraseUserTable();
+    }
+
+    private function populateUserTable()
+    {
+        $conn = $this->app['db'];
+
+        $conn->insert('user', array(
+            'id' => 1,
+            'username' => 'superuser',
+            'password' => 'superuser'
+        ));
+    }
+
+    private function eraseUserTable()
+    {
+        $conn = $this->app['db'];
+        $conn->delete('user', array('id' => 1));
+    }
+}
