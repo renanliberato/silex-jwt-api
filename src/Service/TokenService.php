b6407111c8f15c0958357760490eccdace02cbf4
Renan Liberato <renan.libsantana@gmail.com>
2017-05-17 17:40:29 -0300

    Added signer instantiating to construct process. Added getFromRequest and isAuthorized methods. Added tests to the two created methods.

--- src/Service/TokenService.php
+++ src/Service/TokenService.php
@@ -3,9 +3,13 @@
 namespace App\Service;
 
 use App\Service\ClockInterface;
+use \InvalidArgumentException;
 use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\Parser;
 use Lcobucci\JWT\Signer;
 use Lcobucci\JWT\Signer\Hmac\Sha256;
+use Lcobucci\JWT\Token;
+use Symfony\Component\HttpFoundation\Request;
 
 class TokenService
 {
@@ -24,10 +28,18 @@ class TokenService
      */
     private $key;
 
+    /**
+     * Signer service
+     *
+     * @var Signer
+     */
+    private $signer;
+
     public function __construct(ClockInterface $clock, $key)
     {
-        $this->clock = $clock;
-        $this->key = $key;
+        $this->signer = new Sha256();
+        $this->clock  = $clock;
+        $this->key    = $key;
     }
 
     public function getAccessToken($username)
@@ -37,7 +49,7 @@ class TokenService
             ->setIssuedAt($this->clock->now())
             ->setExpiration($this->clock->plusFiveMinutes())
             ->set('username', $username)
-            ->sign(new Sha256(), $this->key);
+            ->sign($this->signer, $this->key);
 
         return $builder->getToken();
     }
@@ -49,8 +61,58 @@ class TokenService
             ->setIssuedAt($this->clock->now())
             ->setExpiration($this->clock->plusTwentyMinutes())
             ->set('username', $username)
-            ->sign(new Sha256(), $this->key);
+            ->sign($this->signer, $this->key);
 
         return $builder->getToken();
+    }
+
+    /**
+     * Retrieve a Token object from "Authorization" Request header.
+     *
+     * @param Request $request
+     * 
+     * @return null | Token
+     * 
+     * @throws InvalidArgumentException
+     * 
+     */
+    public function getFromRequest(Request $request)
+    {
+        $authorizationHeader = $request->headers->get('Authorization');
+        if (!$authorizationHeader) {
+            return null;
+        }
+
+        $stringToken = str_replace('Bearer ', '', $authorizationHeader);
+
+        if (!$stringToken) {
+            return null;
+        }
+
+        return (new Parser())->parse($stringToken);
+    }
+
+    /**
+     * Check if the token was not modified and is not expired.
+     *
+     * @param Token $token
+     * 
+     * @return boolean
+     */
+    public function isAuthorized(Token $token)
+    {
+        try {
+            if (!$token->verify($this->signer, $this->key)) {
+                return false;
+            }
+
+            if ($token->isExpired()) {
+                return false;
+            }
+
+            return true;
+        } catch (\Exception $e) {
+            return false;
+        }
     }
 }
--- tests/unit/Service/TokenServiceTest.php
+++ tests/unit/Service/TokenServiceTest.php
@@ -4,11 +4,15 @@ namespace AppTest\Service;
 
 use App\Service\ClockInterface;
 use App\Service\TokenService;
+use \InvalidArgumentException;
 use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\Parser;
 use Lcobucci\JWT\Signer\Hmac\Sha256;
 use Lcobucci\JWT\Token;
 use Mockery;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\HeaderBag;
+use Symfony\Component\HttpFoundation\Request;
 
 class TokenServiceTest extends TestCase
 {
@@ -56,5 +60,122 @@ class TokenServiceTest extends TestCase
         $this->assertEquals($renewToken->getClaim('iat'), $now);
         $this->assertEquals($renewToken->getClaim('username'), $username);
         $this->assertTrue($renewToken->verify(new Sha256(), $key));
+    }
+
+    public function testGetFromRequestReturnsNullIfWithoutAuthorizatoinHeader()
+    {
+        // $requestMock->headers = new HeaderBag([
+        //     'Authorization' => 'Bearer ro1i23jr1o23ijr12orij12.1o234j1o23i4j.io123j41o23ij4'
+        // ]);
+
+        $tokenService = new TokenService(Mockery::mock(ClockInterface::class), 'abc');
+
+        $requestMock = Mockery::mock(Request::class);
+        $requestMock->headers = new HeaderBag([]);
+
+        $tokenFromRequest = $tokenService->getFromRequest($requestMock);
+
+        $this->assertNull($tokenFromRequest);
+    }
+
+    public function testGetFromRequestReturnsNullIfWithoutToken()
+    {
+        $tokenService = new TokenService(Mockery::mock(ClockInterface::class), 'abc');
+
+        $requestMock = Mockery::mock(Request::class);
+        $requestMock->headers = new HeaderBag([
+            'Authorization' => ''
+        ]);
+
+        $tokenFromRequest = $tokenService->getFromRequest($requestMock);
+
+        $this->assertNull($tokenFromRequest);
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testGetFromRequestThrowsExceptionIfWithInvalidToken()
+    {
+        $tokenService = new TokenService(Mockery::mock(ClockInterface::class), 'abc');
+        
+        $requestMock = Mockery::mock(Request::class);
+        $requestMock->headers = new HeaderBag([
+            'Authorization' => 'tokenwithoutdots'
+        ]);
+
+        $tokenService->getFromRequest($requestMock);
+    }
+
+    public function testGetFromRequestReturnsTokenObjectIfValidTokenIsPassed()
+    {
+        $tokenService = new TokenService(Mockery::mock(ClockInterface::class), 'abc');
+        
+        $requestMock = Mockery::mock(Request::class);
+        $requestMock->headers = new HeaderBag([
+            'Authorization' => (string)(new Builder())->getToken()
+        ]);
+
+        $token = $tokenService->getFromRequest($requestMock);
+
+        $this->assertNotNull($token);
+        $this->assertInternalType('object', $token);
+        $this->assertInstanceOf(Token::class, $token);
+    }
+
+    public function testIsNotAuthorizedIfAnModifiedTokenIsPassed()
+    {
+        $key = 'abc';
+
+        $tokenService = new TokenService(Mockery::mock(ClockInterface::class), $key);
+        
+        $originalTokenString = (string)(new Builder())
+            ->setExpiration(strtotime('now +1 minute'))
+            ->sign(new Sha256(), $key)
+            ->getToken();
+
+        $originalTokenStringExploded = explode('.', $originalTokenString);
+
+        $claims = json_decode(base64_decode($originalTokenStringExploded[1]));
+
+        $claims->exp = strtotime('now +5 minutes');
+
+        $originalTokenStringExploded[1] = base64_encode(json_encode($claims));
+
+        $modifiedToken = (new Parser())->parse(implode('.', $originalTokenStringExploded));
+
+        $isAuthorized = $tokenService->isAuthorized($modifiedToken);
+
+        $this->assertFalse($isAuthorized);
+    }
+
+    public function testIsNotAuthorizedIfAnExpiredTokenIsPassed()
+    {
+        $key = 'abc';
+        $tokenService = new TokenService(Mockery::mock(ClockInterface::class), $key);
+        
+        $expiredToken = (new Builder())
+            ->setExpiration(strtotime('now -1 minute'))
+            ->sign(new Sha256(), $key)
+            ->getToken();
+
+        $isAuthorized = $tokenService->isAuthorized($expiredToken);
+
+        $this->assertFalse($isAuthorized);
+    }
+
+    public function testIsValidIfAnAuthenticTokenAndNotExpiredIsPassed()
+    {
+        $key = 'abc';
+        $tokenService = new TokenService(Mockery::mock(ClockInterface::class), $key);
+        
+        $expiredToken = (new Builder())
+            ->setExpiration(strtotime('now +1 minute'))
+            ->sign(new Sha256(), $key)
+            ->getToken();
+
+        $isAuthorized = $tokenService->isAuthorized($expiredToken);
+
+        $this->assertTrue($isAuthorized);
     }
 }
