81a28ef1004129be10a8521f59c4cdb1932523c4
Renan Liberato <renan.libsantana@gmail.com>
2017-05-18 17:36:51 -0300

    Added options route to /auth

--- src/routes.php
+++ src/routes.php
@@ -4,6 +4,11 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 $app->before('controller.authorize:process');
+
+$app->options('/auth', function(){
+    return new Response();
+});
+
 $app->post('/auth', "controller.authenticate:process");
 
 $app->error(function (\Exception $e, Request $request, $code) use ($app) {
