7e60f96f16a3c147f550c6adb9a7a68426bc15a3
Renan Liberato <renan.libsantana@gmail.com>
2017-05-18 17:37:54 -0300

    Added CORS headers

--- web/index.php
+++ web/index.php
@@ -9,4 +9,9 @@ require __DIR__.'/../config/prod.php';
 require __DIR__.'/../src/services.php';
 require __DIR__.'/../src/controllers.php';
 require __DIR__.'/../src/routes.php';
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS, HEAD");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 $app->run();
